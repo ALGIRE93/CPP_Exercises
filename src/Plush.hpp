0b0a107307ad420b6e4613a13b518735b9fc9f60
LAGIER RAPHAEL <raphael.lagier@heckel02.u-pem.fr>
2023-01-24 15:13:41 +0100

    Plush with init cost

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -10,8 +10,17 @@ class Plush{
 
         }
 
+        Plush(unsigned int cost)
+            : _cost { cost }
+        {}
+        
+
         unsigned int    get_cost() const {
             return _cost;
+        }
+
+        void            set_cost(unsigned int cost){
+            _cost = cost;
         }
 
 };
