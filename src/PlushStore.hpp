34ab9f041ec2768734d49e559ecb0bc8d1307dd1
LAGIER RAPHAEL <raphael.lagier@heckel02.u-pem.fr>
2023-01-24 15:45:36 +0100

    PlushStore can be more than poor with debt

--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -4,6 +4,9 @@
 class PlushStore{
     private:
         std::string _name;
+        unsigned int _money = 0;
+        unsigned int _stockSize = 0;
+        unsigned int _debt = 0;
     
     public:
         // PlushStore(const std::string& name)
@@ -17,6 +20,23 @@ class PlushStore{
 
         const std::string& get_name() const{
             return _name;
+        }
+
+        unsigned int       get_wealth_amount() const{
+            return _money;
+        }
+
+        unsigned int        get_stock_size() const{
+            return _stockSize;
+        }
+
+        void                loan(unsigned int cash){
+            _money += cash;
+            _debt += cash * 1.1;
+        }
+
+        unsigned int        get_debt_amount() const{
+            return _debt;
         }
 
 };
