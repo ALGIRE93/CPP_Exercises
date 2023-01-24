62509830a3c43240f23173c9f4522c1247f0ea14
LAGIER RAPHAEL <raphael.lagier@heckel02.u-pem.fr>
2023-01-24 15:26:30 +0100

    PlushStore have a Name

--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,1 +1,22 @@
 #pragma once
+#include <iostream>
+
+class PlushStore{
+    private:
+        std::string _name;
+    
+    public:
+        // PlushStore(const std::string& name)
+        //     : _name { name }
+        // {
+
+        // }
+        PlushStore(const std::string name)
+            : _name { name }
+        {}
+
+        const std::string& get_name() const{
+            return _name;
+        }
+
+};
