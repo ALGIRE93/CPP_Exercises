d0ca603e9c6ab2ef030520f83c3c0f94e0c4626c
LAGIER RAPHAEL <raphael.lagier@heckel02.u-pem.fr>
2023-01-24 15:02:24 +0100

    Added Cost of Plush

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
 class Plush{
+    private:
+        unsigned int _cost = 10;
+
     public:
         Plush()
         {
-            
+
+        }
+
+        unsigned int    get_cost() const {
+            return _cost;
         }
 
 };
