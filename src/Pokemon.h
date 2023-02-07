f19792e5664144739f3bb813a6b9a388fd5927a7
Rapha Lagier Algire <raphael39algire@gmail.com>
2023-02-07 06:28:59 -0500

    Pokemon Init

--- src/Pokemon.h
+++ src/Pokemon.h
@@ -1,7 +1,32 @@
 #pragma once
 
 #include <string>
+#include <iostream>
+
+using namespace std;
 
 // A creature that is cute and can fight other ones.
 class Pokemon
-{};
+{
+    private :
+        const string _name;
+        unsigned int _id = 0;
+        unsigned int incr_id = 0;
+    
+    public :
+        Pokemon(const string& name)
+            : _name {name}
+            , _id { incr_id++ }
+        {}
+
+        Pokemon(const Pokemon& other)
+            : _name {other._name}
+        {}
+
+        string name() const{
+            return _name;
+        }
+
+        
+
+};
