c5aa58fda3e385054343a3e451acd7f352e4bf09
Rapha Lagier Algire <raphael39algire@gmail.com>
2023-02-07 06:42:49 -0500

    Pokeball init

--- src/Pokeball.h
+++ src/Pokeball.h
@@ -1,5 +1,24 @@
 #pragma once
 
+#include <iostream>
+#include <Pokemon.h>
+
+using namespace std;
+
 // A ball where a Pokemon sleeps.
 class Pokeball
-{};
+{
+    private:
+        bool _empty = true;
+
+    public: 
+        Pokeball()
+        {}
+
+        bool empty() const{
+            return _empty;
+        }
+
+
+
+};
