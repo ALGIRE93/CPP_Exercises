71c1b161785a995e1d2943ebf1e0773d24afe7c9
LAGIER RAPHAEL <raphael.lagier@heckel02.u-pem.fr>
2023-02-21 14:50:52 +0100

    Test 1 passed TP6

---
+++ src/Leaf.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Node.hpp"
+
+using namespace std;
+
+class Leaf : public Node
+{
+    private :
+
+    protected :
+        Leaf();
+
+    public :
+
+};
--- src/Node.hpp
+++ src/Node.hpp
@@ -1,1 +1,18 @@
 #pragma once
+
+#include <iostream>
+#include "InstanceCounter.hpp"
+
+using namespace std;
+
+class Node : public InstanceCounter
+{
+    private :
+
+    protected :
+        Node();
+
+    public :
+
+};
+
