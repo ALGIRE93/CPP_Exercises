0768259301a58c197b56ce48f568e11268fca0fd
Rapha Lagier Algire <raphael39algire@gmail.com>
2023-02-28 08:03:59 -0500

    return to tp4

--- src/Pokeball.h
+++ src/Pokeball.h
@@ -6,24 +6,24 @@
 using namespace std;
 
 // A ball where a Pokemon sleeps.
-class Pokeball
-{
-    private:
-        PokemonPtr _pokemon;
+// class Pokeball
+// {
+//     private:
+//         PokemonPtr _pokemon;
 
-    public: 
-        const Pokemon& pokemon() const{
-            return *_pokemon;
-        }
+//     public: 
+//         const Pokemon& pokemon() const{
+//             return *_pokemon;
+//         }
 
-        bool empty() const{
-            return _pokemon == nullptr;
-        }
+//         bool empty() const{
+//             return _pokemon == nullptr;
+//         }
 
-        void store(PokemonPtr pokemon){
-            _pokemon = move(pokemon);
-        }
+//         void store(PokemonPtr pokemon){
+//             _pokemon = move(pokemon);
+//         }
 
 
 
-};
+// };
