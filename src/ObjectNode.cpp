49901e1a118a62b7853d2255367231336d2f9a9b
LAGIER RAPHAEL <raphael.lagier@heckel02.u-pem.fr>
2023-03-01 11:45:30 +0100

    Test 5 ObjectNode

--- src/ObjectNode.cpp
+++ src/ObjectNode.cpp
@@ -1,1 +1,10 @@
 #include "ObjectNode.hpp"
+
+using namespace std;
+
+string ObjectNode::print() const{
+    string result = "{";
+
+    result += "}";
+    return result;
+}
--- src/ObjectNode.hpp
+++ src/ObjectNode.hpp
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <string>
+#include "Node.hpp"
+
+using namespace std;
+
+class ObjectNode : public Node
+{
+    private :
+
+    public :
+        ObjectNode()
+            : Node(NodeKind::OBJECT)
+        {}
+
+        string print() const override;
+
+};
