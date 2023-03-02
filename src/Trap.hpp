6ce1a442f7534dc8c62ba12308d05f98c4aa3f0e
Rapha Lagier Algire <raphael39algire@gmail.com>
2023-03-02 15:56:54 -0500

    TP5, test7 valid

--- src/Trap.hpp
+++ src/Trap.hpp
@@ -1,1 +1,48 @@
 #pragma once
+
+#include <string>
+#include "Card.hpp"
+
+using namespace std;
+
+
+enum class TrapType
+{
+    Normal,
+    Continuous,
+    Counter
+};
+
+string to_string(TrapType trapType)
+{
+    switch(trapType)
+    {
+        case TrapType::Normal:
+            return "Normal";
+        case TrapType::Continuous:
+            return "Continuous";
+        case TrapType::Counter:
+            return "Counter";
+        default:
+            return "";
+    };
+
+}
+
+class Trap : public Card
+{
+    private :
+        TrapType _trapType;
+
+    public :
+        Trap( string id, string name, TrapType trapType)
+            : Card { id, CardType::Trap }
+            , _trapType { trapType }
+        {
+            set_name( name );
+        }
+
+        TrapType get_trap_type(){
+            return _trapType;
+        }
+};
