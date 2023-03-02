723c7808a742d89bf9b28faa1d383f1bf8b40a71
Rapha Lagier Algire <raphael39algire@gmail.com>
2023-03-02 12:33:06 -0500

    TP5, test5 valid, continue

--- src/Card.hpp
+++ src/Card.hpp
@@ -5,7 +5,68 @@
 using namespace std;
 
 
+enum class CardType 
+{
+    Monster,
+    Spell,
+    Trap
+};
 
+string to_string(CardType type)
+{
+    switch(type)
+    {
+        case CardType::Monster:
+            return "Monster";
+        case CardType::Spell:
+            return "Spell";
+        case CardType::Trap:
+            return "Trap";
+        default:
+            return "";
+    };
+}
+
+class Card
+{
+    private :
+        string _id;
+        CardType _cardType;
+        string _name;
+        string _description;
+        
+    public :
+        Card(string id, CardType cardType)
+            : _id { id }
+            , _cardType { cardType }
+        {}
+
+        string get_id() const{
+            return _id;
+        }
+
+        CardType get_type() const{
+            return _cardType;
+        }
+
+        string get_name() const{
+            return _name;
+        }
+
+        string get_description() const{
+            return _description;
+        }
+
+        void set_name(string name){
+            _name = name;
+        }
+
+        void set_description(string description){
+            _description = description;
+        }
+
+
+};
 
 
 
--- src/Monster.hpp
+++ src/Monster.hpp
@@ -4,6 +4,81 @@
 #include <string>
 #include <iostream>
 
+
+using namespace std;
+
+enum class Attribute
+{
+    Dark,
+    Divine,
+    Earth,
+    Fire,
+    Light,
+    Water,
+    Wind
+};
+
+string to_symbol(Attribute attribute)
+{
+    switch(attribute)
+    {
+        case Attribute::Dark:
+            return u8"闇";
+        case Attribute::Divine:
+            return u8"神";
+        case Attribute::Earth:
+            return u8"地";
+        case Attribute::Fire:
+            return u8"炎";
+        case Attribute::Light:
+            return u8"光";
+        case Attribute::Water:
+            return u8"水";
+        case Attribute::Wind:
+            return u8"風";
+        default:
+            return "";
+    };
+}
+
+class Monster : public Card
+{
+    private :
+        Attribute _attribute;
+        string _monster_type;
+        int _atk;
+        int _def;
+
+    public :
+        Monster(string id, string name, Attribute attribute, string monster_type, int atk, int def)
+            : Card {id, CardType::Monster}
+            , _attribute { attribute }
+            , _monster_type { monster_type }
+            , _atk { atk }
+            , _def { def }
+        {
+            set_name(name);
+        }
+
+        Attribute get_attribute() const{
+            return _attribute;
+        }
+
+        int get_atk() const{
+            return _atk;
+        }
+
+        int get_def() const{
+            return _def;
+        }
+
+        string get_description() const{
+            return "["+_monster_type + "]\n" + Card::get_description() + "\n"
+            +"ATK/"+ to_string(_atk) + " DEF/" + to_string(_def);
+        }
+
+};
+
 // using namespace std;
 
 // enum Attribute {Dark, Divine, Earth, Fire, Light, Water, Wind};
