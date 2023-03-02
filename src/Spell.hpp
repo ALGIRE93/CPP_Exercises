cea137f0255ed6fdb430896822782ab74c514171
Rapha Lagier Algire <raphael39algire@gmail.com>
2023-03-02 15:50:36 -0500

    TP5, test6 valid

--- src/Spell.hpp
+++ src/Spell.hpp
@@ -1,1 +1,52 @@
 #pragma once
+
+#include <string>
+#include "Card.hpp"
+
+using namespace std;
+
+enum class SpellType
+{
+    Normal,
+    Equip,
+    Continuous,
+    QuickPlay,
+    Field
+};
+
+string to_string(SpellType spellType)
+{
+    switch(spellType)
+    {
+        case SpellType::Normal:
+            return "Normal";
+        case SpellType::Equip:
+            return "Equip";
+        case SpellType::Continuous:
+            return "Continuous";
+        case SpellType::QuickPlay:
+            return "Quick-Play";
+        case SpellType::Field:
+            return "Field";
+        default:
+            return "";
+    };
+}
+
+class Spell : public Card
+{
+    private :
+        SpellType _spellType;
+
+    public :
+        Spell(string id, string name, SpellType spellType)
+            :Card { id, CardType::Spell}
+            , _spellType { spellType }
+        {
+            set_name( name );
+        }
+
+        SpellType get_spell_type(){
+            return _spellType;
+        }
+};
