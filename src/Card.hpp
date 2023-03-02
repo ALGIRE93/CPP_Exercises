770d45989ea3fb432d364ffc187c1e1694efef96
Rapha Lagier Algire <raphael39algire@gmail.com>
2023-03-02 10:32:49 -0500

    Retry du TP5

--- src/Card.hpp
+++ src/Card.hpp
@@ -5,55 +5,80 @@
 using namespace std;
 
 
-enum class CardType { Monster, Spell, Trap};
 
-string to_string(CardType cardType){
-    string res ="";
-    switch (cardType)
-    {
-    case CardType::Monster : return "Monster";
-    case CardType::Spell: return "Spell";
-    case CardType::Trap : return "Trap";
-    default:return "";
-    }
-};
 
-class Card {
-    private :
-        string _id;
-        CardType _cardType;
-        string _name = "";
-        string _description = "";
 
-    public :
-        Card(string id, CardType cardType)
-        : _id { id }
-        , _cardType { cardType }
-        {}
 
-        string get_id() const{
-            return _id;
-        }
 
-        CardType get_type() const{
-            return _cardType;
-        }
 
-        string get_name() const{
-            return _name;
-        }
 
-        string get_description() const{
-            return _description;
-        }
 
-        void set_name(string name){
-            _name = name;
-        }
 
-        void set_description(string description){
-            _description = description;
-        }
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// enum class CardType { Monster, Spell, Trap};
+
+// string to_string(CardType cardType){
+//     switch (cardType)
+//     {
+//     case CardType::Monster : return "Monster";
+//     case CardType::Spell: return "Spell";
+//     case CardType::Trap : return "Trap";
+//     default:return "";
+//     }
+// };
+
+// class Card {
+//     private :
+//         string _id;
+//         CardType _cardType;
+//         string _name = "";
+//         string _description = "";
+
+//     public :
+//         Card(string id, CardType cardType)
+//         : _id { id }
+//         , _cardType { cardType }
+//         {}
+
+//         string get_id() const{
+//             return _id;
+//         }
+
+//         CardType get_type() const{
+//             return _cardType;
+//         }
+
+//         string get_name() const{
+//             return _name;
+//         }
+
+//         string get_description() const{
+//             return _description;
+//         }
+
+//         void set_name(string name){
+//             _name = name;
+//         }
+
+//         void set_description(string description){
+//             _description = description;
+//         }
+// };
 
 
--- src/Monster.hpp
+++ src/Monster.hpp
@@ -4,80 +4,80 @@
 #include <string>
 #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-enum Attribute {Dark, Divine, Earth, Fire, Light, Water, Wind};
+// enum Attribute {Dark, Divine, Earth, Fire, Light, Water, Wind};
 
-string to_symbol(Attribute attribute){
-    string res = "";
-    switch (attribute)
-    {
-    case Dark/* constant-expression */:
-        /* code */
-        res = u8"闇";
-        break;
+// string to_symbol(Attribute attribute){
+//     string res = "";
+//     switch (attribute)
+//     {
+//     case Dark/* constant-expression */:
+//         /* code */
+//         res = u8"闇";
+//         break;
     
-    case Divine:
-        res = u8"神";
-        break;
+//     case Divine:
+//         res = u8"神";
+//         break;
     
-    case Earth:
-        res = u8"地";
-        break;
+//     case Earth:
+//         res = u8"地";
+//         break;
 
-    case Fire:
-        res = u8"炎";
-        break;
+//     case Fire:
+//         res = u8"炎";
+//         break;
     
-    case Light:
-        res = u8"光";
-        break;
+//     case Light:
+//         res = u8"光";
+//         break;
 
-    case Water:
-        res = u8"水";
-        break;
+//     case Water:
+//         res = u8"水";
+//         break;
 
-    case Wind:
-        res = u8"風";
-        break;
+//     case Wind:
+//         res = u8"風";
+//         break;
     
-    default:
-        break;
-    }
-    return res;
-}
+//     default:
+//         break;
+//     }
+//     return res;
+// }
 
-class Monster : public Card{
-    private :
-        Attribute _attribute = {};
-        string _category;
-        const unsigned int _atk;
-        const unsigned int _def;
+// class Monster : public Card{
+//     private :
+//         Attribute _attribute = {};
+//         string _category;
+//         const unsigned int _atk;
+//         const unsigned int _def;
 
 
-    public :
-        Monster(string id, string name, Attribute attribute, string category, const unsigned int atk, const unsigned int def)
-            :Card { id , CardType::Monster}
-            , _attribute {attribute}
-            , _category {category}
-            , _atk {atk}
-            , _def {def}
-        {
-            set_name(name);
-        }
+//     public :
+//         Monster(string id, string name, Attribute attribute, string category, const unsigned int atk, const unsigned int def)
+//             :Card { id , CardType::Monster}
+//             , _attribute {attribute}
+//             , _category {category}
+//             , _atk {atk}
+//             , _def {def}
+//         {
+//             set_name(name);
+//         }
 
-        Attribute get_attribute(){
-            return _attribute;
-        }
+//         Attribute get_attribute(){
+//             return _attribute;
+//         }
 
-        unsigned int get_atj(){
-            return _atk;
-        }
+//         unsigned int get_atj(){
+//             return _atk;
+//         }
 
-        unsigned int get_def(){
-            return _def;
-        }
+//         unsigned int get_def(){
+//             return _def;
+//         }
         
         
 
-};
+// };
