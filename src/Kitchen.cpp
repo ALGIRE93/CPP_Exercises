6d1d380dd490d647b25b035b07909ab3a5d68480
LAGIER RAPHAEL <raphael.lagier@heckel02.u-pem.fr>
2023-03-14 15:46:20 +0100

    Bonjour le TP8 est relou

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -1,1 +1,21 @@
 #include "Kitchen.hpp"
+
+#include <utility>
+
+//auto it = units.emplace(units.end(), move(unit));
+//return *it; // avant la ref wrap *it
+
+const Unit& Kitchen::register_unit(Unit unit)
+{
+    units.emplace_back(unit);
+    return units.back();
+}
+
+const Unit* Kitchen::find_unit(const string& name) const{
+    for(auto it = units.begin(); it != units.end(); it++){
+        if(it->get().name == name){ // it->name
+            return &(it->get()); // &(*it)
+        }
+    }
+    return nullptr;
+}
--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,25 @@
 #pragma once
 
+#include "../lib/Unit.hpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <optional>
+#include <functional>
+
+
+using namespace std;
+
 class Kitchen
-{};
+{
+    private :
+        vector<reference_wrapper<Unit>> units; // équivalent à un vecteur qui a des refs
+
+    public :
+
+        const Unit& register_unit(Unit unit);
+
+        const Unit* find_unit(const string& name) const;
+        
+};
