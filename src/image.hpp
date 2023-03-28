cbccc844f28c118067d0b96e58e360625f5edac7
LAGIER RAPHAEL <raphael.lagier@heckel02.u-pem.fr>
2023-03-28 15:18:08 +0200

    TP10 test1 ok

--- src/image.hpp
+++ src/image.hpp
@@ -12,3 +12,21 @@
 #include <iostream>
 
 using namespace std;
+
+template<typename P, size_t W, size_t H>
+struct Image{
+    private :
+        array<array<P, W>, H> _data;
+    public :
+        Image(const P& pix)
+        {
+            for(size_t j = 0; j < H; j++){
+                for(size_t i = 0; i < W; i++){
+                    _data[j][i] = pix;
+                }
+            }
+        }
+
+        Image() = default;
+
+};
