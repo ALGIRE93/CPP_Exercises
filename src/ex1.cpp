fa3ad5ca548ea39013839714ed6685e00f8e097b
LAGIER RAPHAEL <raphael.lagier@heckel02.u-pem.fr>
2023-04-04 15:07:04 +0200

    TP11 exo1

--- src/ex1.cpp
+++ src/ex1.cpp
@@ -1,1 +1,11 @@
 #include "ex1.hpp"
+
+void pairwise_concatenate(list<list<int>>& list1, const list<list<int>>& list2){
+    // List de concaténation de list
+    auto it1 = list1.begin();
+    for(auto it2 = list2.begin(); it2 != list2.end(); it2++){
+        concatenate(*it1, *it2); //value de l'iterator 1 et 2 concate
+        it1++;
+    }
+}
+
--- src/ex1.hpp
+++ src/ex1.hpp
@@ -1,1 +1,8 @@
 #pragma once
+#include <list>
+#include "../lib/concatenate.hpp"
+
+using namespace std;
+
+void pairwise_concatenate(list<list<int>>& list1, const list<list<int>>& list2);
+
